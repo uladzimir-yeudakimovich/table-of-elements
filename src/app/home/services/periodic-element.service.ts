4505811a8284b58fa449061f31f7c438d485f87f
ULADZIMIR YEUDAKIMOVICH <uladzimir.yeudakimovich@gmail.com>
2024-09-18 23:23:06 +0200

    refactor: remove not use code

--- src/app/home/services/periodic-element.service.ts
+++ src/app/home/services/periodic-element.service.ts
@@ -1,18 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, delay, Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
 import { PeriodicElement } from '../models/periodic-element.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PeriodicElementService {
-  private periodicElementsSubject = new BehaviorSubject<PeriodicElement[]>([]);
-  periodicElements$ = this.periodicElementsSubject.asObservable();
-
-  private filterKeywordSubject = new BehaviorSubject<string>('');
-  filterKeyword$ = this.filterKeywordSubject.asObservable();
-
   private apiUrl = 'data/elements.json';
 
   constructor(private http: HttpClient) { }
